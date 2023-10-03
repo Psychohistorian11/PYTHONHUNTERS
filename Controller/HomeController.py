12ae3552b4080f805a546d6cad886d284a432286
Jevdev2304 <jvaldes245@soyudemedellin.edu.co>
2023-10-03 13:12:07 -0500

    Creacion rama Juanfer

--- Controller/HomeController.py
+++ Controller/HomeController.py
@@ -55,13 +55,8 @@ class HomeController:
 
     @app.route("/LoginEmailandPassword", methods=["POST"])
     def LoginE_and_P(self=None):
-<<<<<<< HEAD
         edd =0 
         listThemesFromDB = DB.get_themesDB(edd)
-=======
-        idCourse = 0
-        listThemesFromDB = DB.get_themesDB(idCourse)
->>>>>>> 88f123736f117beae2c5a4b1bca7043a1318e9ea
         ThemeObject.update_themes(listThemesFromDB)
         if request.method == "POST":
             email = request.form["loginEmail"]
